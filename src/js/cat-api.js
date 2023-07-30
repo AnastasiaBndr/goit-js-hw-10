ec004306efcd67b50bf810343959bf83d609bfe7
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-07-30 18:12:28 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -9,10 +9,8 @@ const URL = `https://api.thecatapi.com/v1/breeds`;
 export async function fetchBreed() {
     try {
       const res = await axios.get(URL);
-      console.log(res.data);
-      return res;
-      // const сatsBreeds = res.data.filter(cat => cat.image?.url != null || cat.id === 'mala');
-      // return сatsBreeds
+      const сatsBreeds = res.data.filter(cat => cat.image?.url != null || cat.id === 'mala');
+      return сatsBreeds
     } catch (error) {
       Notiflix.Report.info("Error!", "Cats ran away somewhere..", "Ok");
       throw error;
