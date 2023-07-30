d390c6472fac0becc576a7611b9e4a8864fe1154
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-07-30 18:04:09 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
 import 'regenerator-runtime/runtime';
-import {axios} from 'axios';
+import axios from 'axios';
 
 axios.defaults.headers.common["x-api-key"] = "live_8dKphs0W18lTFRIxlVkHb5ILtwEMbmXoQKgXYGOIwMxvPTJidJBFw8JRSAFCCT2p";
 
@@ -8,7 +8,7 @@ const URL = `https://api.thecatapi.com/v1/breeds`;
 
 export async function fetchBreed() {
     try {
-      const res = axios.get(URL);
+      const res = await axios.get(URL);
       const сatsBreeds = res.data.filter(cat => cat.image?.url != null || cat.id === 'mala');
       return сatsBreeds
     } catch (error) {
