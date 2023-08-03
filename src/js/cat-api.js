f847551d2c4f4a7af82634a7becdd185b5d523f3
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-08-03 20:57:46 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -8,6 +8,7 @@ const URL = `https://api.thecatapi.com/v1/breeds/?`;
 
 export async function fetchBreed() {
 
+      console.log(URL+API_KEY);
       return axios.get(URL+API_KEY)
       .then(resp=>{return resp.data})
       .then(cats=>{return cats.filter(cat => cat.image?.url != null || cat.id === 'mala')})
