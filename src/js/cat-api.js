ecdcc4fb308036fe3c5c53fa4dd8a5297f3a4def
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-08-03 20:36:40 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,15 +2,19 @@ import Notiflix from 'notiflix';
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
-const API_KEY = "?key=live_8dKphs0W18lTFRIxlVkHb5ILtwEMbmXoQKgXYGOIwMxvPTJidJBFw8JRSAFCCT2p";
+axios.defaults.headers.common['x-api-key'] =
+  '?api_key=live_8dKphs0W18lTFRIxlVkHb5ILtwEMbmXoQKgXYGOIwMxvPTJidJBFw8JRSAFCCT2p';
 
 const URL = `https://api.thecatapi.com/v1/breeds`;
 
 export async function fetchBreed() {
-
-      return axios.get(URL+API_KEY)
-      .then(resp=>{return resp.data})
-      .then(cats=>{return cats.filter(cat => cat.image?.url != null || cat.id === 'mala')})
-      .catch(Notiflix.Report.info("Error!", "Cats ran away somewhere..", "Ok"))
-    
-  }
+  return axios
+    .get(URL)
+    .then(resp => {
+      return resp.data;
+    })
+    .then(cats => {
+      return cats.filter(cat => cat.image?.url != null || cat.id === 'mala');
+    })
+    .catch(Notiflix.Report.info('Error!', 'Cats ran away somewhere..', 'Ok'));
+}
